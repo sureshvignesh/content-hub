a4c8303561672f75d5ad0372720f80230f85673d
Vignesh Suresh <pingviky@gmail.com>
2018-09-04 17:52:14 +0530

    Handle no file request

--- app/services/validate_upload_service.rb
+++ app/services/validate_upload_service.rb
@@ -8,6 +8,7 @@ class ValidateUploadService
   end
 
   def validate
+    return {error: 'No File found', status: 422} unless @file.present?
     return {error: 'File format mismatch - File formats are limited to bitmaps: JPG and PNG', status: 422} unless FILE_FORMATS.include? @file.content_type.chomp
     dimensions = eval(%x(identify -format '[%w,%h]' #{@file.tempfile.path}))
     return {error: 'File dimension mismatch - Files are limited to a minimum of 350x350 and a maximum of 5000x5000 size', status: 422} unless (dimensions.min >= MIN_DIMENSION && dimensions.max <= MAX_DIMENSION)
